ae978ae64cf2fd748c58c886cc247e8a5089b423
Kartik213 <kartikg02013@gmail.com>
2025-12-26 23:42:38 +0530

    removed summarize from useEffect dependency

--- app/tx/[digest]/page.tsx
+++ app/tx/[digest]/page.tsx
@@ -46,7 +46,7 @@ export default function TxPage() {
     }
 
     fetchTx();
-  }, [digest, summarize]);
+  }, [digest]);
 
   async function summarizeMoveCalls(txs: any[]) {
     const arr: any[] = [];
